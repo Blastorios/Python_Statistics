40f801fa4e583f108a4a26e6dfcd9bdf9d4c742f
Blastor1os <53938178+Blastorios@users.noreply.github.com>
2020-01-17 11:11:19 +0100

    Update removing_outliers.py

--- removing_outliers.py
+++ removing_outliers.py
@@ -1,30 +1,35 @@
 ###Full credits to @author: ddo033
 
 ###(ALREADY WORKING)
-def scrape_data_for_plot_IQR(df, col, group_col, cut_off = 3):
+def quantiles_per_column_per_group(df, col, group_col, cut_off = 1.5):
     
     ##df = the DataFrame you want to scan through
     ##col = the column with the float/int values to do the pd.quantile over
     ##group_col = the column which holds the groups that you want to divide col's values from
     ##cut_ff = the StdDev range you wish to apply to the dataset (generally '3' is regarded as an outlier)
     
-    dfs_to_plot = []
-    for group in df[group_col].unique():
-        temp_df = pd.DataFrame()
-        data_per_group = df[col][df[group_col] == group]
+    dfc = df.copy()
+    for group in dfc[group_col].unique():
+        data_per_group = dfc[col][dfc[group_col] == group]
         
         Q1 = data_per_group.quantile(0.25)
         Q3 = data_per_group.quantile(0.75)
         IQR = Q3 - Q1
         
-        data_per_group = data_per_group[~((data_per_group < (Q1 - cut_off * IQR)) | (data_per_group > (Q3 + cut_off * IQR)))]
+        lowOutlier = Q1 - cut_off * IQR
         
-        print(f"min = {Q1-cut_off*IQR}, max = {Q3+cut_off*IQR}")
+        highOutlier = Q3 + cut_off * IQR
         
-        temp_df[col] = data_per_group
-        temp_df[group_col] = group
-        dfs_to_plot.append(temp_df)
-    return pd.concat(dfs_to_plot)
+        for values in data_per_group:
+            if values >= lowOutlier:
+                if values <= highOutlier:
+                    pass
+                else:
+                    dfc.loc[dfc[col] == values, col] = np.nan
+            else:
+                dfc.loc[dfc[col] == values, col] = np.nan
+    
+    return dfc
 
 
 ###(NOT WORKING YET)
